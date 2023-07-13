60b4eb8561104294d5627e2ca4b46adb8fdc8974
Mercy <mercy.mwongeli@student.moringaschool.com>
2023-07-13 10:14:46 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Code your solutions in this file
 
-function writeCards(names, event) {
-    var messages = [];
-    for (var i = 0; i < names.length; i++) {
-      var message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
-      messages.push(message);
-    }
-    return messages;
-  }
+//function writeCards(names, event) {
+    //var messages = [];
+  //  for (var i = 0; i < names.length; i++) {
+   //   var message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+   //   messages.push(message);
+   // }
+ //   return messages;
+ // }
   
-  var result = writeCards(["Charlie", "Samip", "Ali"], "birthday");
-  console.log(result);
+ // var result = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+ // console.log(result);
+ function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
   
